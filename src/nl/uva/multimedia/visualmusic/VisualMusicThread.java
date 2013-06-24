d44fbf28e1d7c0456b19c0af5089283475d363a1
Mats ten Bohmer <mtenbohmer@gmail.com>
2013-06-24 15:34:56 +0200

    Merge

--- src/nl/uva/multimedia/visualmusic/VisualMusicThread.java
+++ src/nl/uva/multimedia/visualmusic/VisualMusicThread.java
@@ -12,9 +12,9 @@ public class VisualMusicThread extends FingerThread {
     private int i = 0;
 //    private PlayTone mPlayTone = null;
 
-    public static final int N_PARTICLE_GROUPS = 50;
-    public static final int PARTICLE_GROUP_SIZE = 20;
-    private static final int PARTICLE_AMOUNT = 50;
+    public static final int N_PARTICLE_GROUPS = 50; /* Total number of particle-groups */
+    public static final int PARTICLE_GROUP_SIZE = 20; /* Number of unique particles in a single group */
+    private static final int PARTICLE_AMOUNT = 50; /* Same as N_PARTICLE_GROUPS ? */
 
     Particles[] particles = new Particles[PARTICLE_AMOUNT];
 
@@ -37,10 +37,14 @@ public class VisualMusicThread extends FingerThread {
             return;
 
         int newX = (int)monitor.getX();
-        if (newX != this.lastX) {
-             particles[this.i++ % PARTICLE_AMOUNT] =
-                     new Particles(PARTICLE_GROUP_SIZE, this.monitor.getX(),
-                     this.monitor.getY(), 5, 5,200);
+
+        /* The 1 in this if statement can be changed to a higher setting if
+         * it is decided that verticle movement is not allowed. */
+        if (Math.abs(newX - this.lastX) >= 1) {
+            particles[this.i++ % PARTICLE_AMOUNT] =
+                    new Particles(PARTICLE_GROUP_SIZE, this.monitor.getX(),
+                    this.monitor.getY(), 5, 5, 200);
+            this.lastX = newX;
         }
 
         renderFrame(monitor);
