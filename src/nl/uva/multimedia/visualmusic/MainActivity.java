f988345d91396e89b4863c764f17aa8e8759f5a4
Mats ten Bohmer <mtenbohmer@gmail.com>
2013-06-25 21:49:05 +0200

    Merge

--- src/nl/uva/multimedia/visualmusic/MainActivity.java
+++ src/nl/uva/multimedia/visualmusic/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends MultitouchActivity {
 
     private static final String TAG = "MainActivity";
 
-    public static final int N_FINGER_THREADS = 1;
+    public static final int N_FINGER_THREADS = 10;
 
     private RelativeLayout mRootLayout = null;
     private FingerHandler<VisualMusicThread, VisualMusicThreadMonitor> mFingerHandler = null;
