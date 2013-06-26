91df070682c108d609f8d19bb74faad60110f999
Mats ten Bohmer <mtenbohmer@gmail.com>
2013-06-26 15:19:44 +0200

    Updated color schemes

--- src/nl/uva/multimedia/visualmusic/VisualMusicThreadMonitor.java
+++ src/nl/uva/multimedia/visualmusic/VisualMusicThreadMonitor.java
@@ -78,7 +78,7 @@ public class VisualMusicThreadMonitor extends FingerThreadMonitor {
 
     public void pick_color_scheme(){
         Random r = new Random();
-        int id = (r.nextInt(9) +0);
+        int id = (r.nextInt(4) +0);
 
         this.begin_color    = createBeginColor(id);
         this.end_color      = createEndColor(id);
@@ -91,19 +91,19 @@ public class VisualMusicThreadMonitor extends FingerThreadMonitor {
             case 1:
                 return Color.argb(255, 0, 94, 255); /* Medium blue */
             case 2:
-                return Color.argb(255, 100, 227, 18); /* Green with blue tint */
+                return Color.argb(255, 0, 204, 102); /* Aqua green */
             case 3:
                 return Color.argb(255, 232, 232, 232); /* Almost white */
             case 4:
-                return Color.argb(255, 0, 255, 255);
+                return Color.argb(255, 255, 255, 0); /* Dark grey */
             case 5:
-                return Color.argb(255, 255, 0, 255);
+                return Color.argb(255, 255, 0, 255); /* Purple-pink */
             case 6:
-                return Color.argb(255, 255, 0, 0);
+                return Color.argb(255, 255, 0, 72); /* Purple-red */
             case 7:
-                return Color.argb(255, 0, 255, 0);
+                return Color.argb(255, 20, 15, 64); /* Dark blue */
             case 8:
-                return Color.argb(255, 0, 0, 255);
+                return Color.argb(255, 150, 89, 0);
             case 9:
                 return Color.argb(255, 255, 255, 0);
             default:
@@ -116,21 +116,21 @@ public class VisualMusicThreadMonitor extends FingerThreadMonitor {
             case 0:
                 return Color.argb(255, 255, 255, 0); /* Yellow */
             case 1:
-                return Color.argb(255, 0, 185, 235); /* Nearing aqua */
+                return Color.argb(255, 0, 201, 255); /* Nearing aqua */
             case 2:
-                return Color.argb(255, 183, 255, 0); /* Pretty bright green */
+                return Color.argb(255, 0, 204, 41); /* Light green-ish */
             case 3:
                 return Color.argb(255, 142, 145, 148); /* Gray (with slight blue) */
             case 4:
-                return Color.argb(255, 255, 0, 255);
+                return Color.argb(255, 255, 255, 255); /* Dark red / brown */
             case 5:
-                return Color.argb(255, 255, 0, 0);
+                return Color.argb(255, 255, 0, 0); /* Red */
             case 6:
-                return Color.argb(255, 0, 255, 0);
+                return Color.argb(255, 131, 117, 255); /* Violet */
             case 7:
-                return Color.argb(255, 0, 0, 255);
+                return Color.argb(255, 85, 179, 185); /* Type of light blue */
             case 8:
-                return Color.argb(255, 255, 255, 0);
+                return Color.argb(255, 232, 217, 0); /* Kind of yellow */
             case 9:
                 return Color.argb(255, 255, 0, 0);
             default:
